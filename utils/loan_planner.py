6600365a941ce750feed1146a720bfc2bbb1ac78
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-25 22:33:15 +0530

    Update loan_planner.py

--- utils/loan_planner.py
+++ utils/loan_planner.py
@@ -6,26 +6,29 @@ from dotenv import load_dotenv
 load_dotenv()
 def data_input(principal, rate, time, income):
   loan_calc=compound_interest_calculation(principal, rate, time)
-  emi_calc=emi_calculation(principal, rate, time)
+  emi_calc=emi_calculation(principal, rate, time, income)
   emi=emi_calc.get("EMI",0)
   check=financial_check(emi, income)
   return {"Loan_Amount":loan_calc.get("Amount",0),
           "Loan_Interest":loan_calc.get("Interest",0),
           "EMI":emi,
           "Net_take_home":emi_calc.get("Net_take_income",0),
-          "Ratio":check.get("Ratio",0)
+          "Ratio":check.get("Ratio",0),
           "Zone":check.get("Zone",0)
   }
 
 def compound_interest_calculation(principal, rate, time):#rate per annum, time in years, amount in rupees
-  amt=principal*((1+(rate/100))**T)
+  amt=principal*((1+(rate/100))**time)
   interest=amt-principal
   return {"Amount":amt,"Interest":interest}
 
-def emi_calculation(principal, rate, time):
-  m_rate=rate/12
+def emi_calculation(principal, rate, time, income):
+  m_rate=rate/12*100
   m_time=time*12
-  emi= principal* m_rate *(((1+m_rate)**m_time)/((1+m_rate)**m_time)-1))
+  try:
+    emi= (principal* m_rate *(1+m_rate)**m_time))/((1+m_rate)**m_time)-1)
+  except Exception as e:
+    emi=0
   net=income-emi
   return {"EMI":emi,"Net_take_home":net}
   
@@ -44,11 +47,12 @@ message=data_input(principal, rate, time, income)
 
 def financial_advice(message):
     try:
+        user_prompt_string = json.dumps(metrics_dict, indent=2)
         res = client.chat.completions.create(
             model="llama-3.1-8b-instant",
             messages=[
                 {"role": "system", "content": "You are a loan advisor, based on the info provided, give the insights whether the user should take a loan or not. What should they improve to make the loan suitable for them? Be precise and accurate."},
-                {"role": "user", "content": message}
+                {"role": "user", "content": f"Here are the financial metrics:{user_prompt_string}"}
             ]
         )
 
