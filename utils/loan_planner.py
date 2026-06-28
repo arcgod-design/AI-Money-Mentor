222ca51989c77f3fdbea5142b92e6b32744f6d38
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-06-28 13:19:58 +0530

    Update loan_planner.py

--- utils/loan_planner.py
+++ utils/loan_planner.py
@@ -66,7 +66,7 @@ def financial_advice(message):
         res = client.chat.completions.create(
             model="llama-3.1-8b-instant",
             messages=[
-                {"role": "system", "content": "You are a loan advisor, based on the info provided, provide the answers to the following questions:Should the user take the loan or not? What should they improve to make the loan suitable for them? All the prices are in rupees.For zone, 1 is safe, 0 is moderate and -1 is risky. Be precise and accurate."},
+                {"role": "system", "content": "You are a loan advisor, based on the info provided, provide the answers to the following questions:Are there any risks associated with the loan? What should they improve to make the loan suitable for them? All the prices are in rupees.For zone, 1 is safe, 0 is moderate and -1 is risky. Be precise and accurate."},
                 {"role": "user", "content": f"Here are the financial metrics:{user_prompt_string}"}
             ]
         )
